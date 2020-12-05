a257859837ff750a5941e27c3c9bf5ea430a45d4
al-co-js <shimjs8@naver.com>
2020-12-05 17:51:13 +0900

    feat: increase capacity limit to 30mb

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,8 @@ import routes from './routes';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: false, limit: '30mb' }));
 app.use(cookieParser());
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
