fd60eae3cf712d8cfde22b1d1d2af91f5607e766
al-co-js <shimjs8@naver.com>
2020-12-05 19:32:38 +0900

    feat: jwt handler type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,38 @@
+import { Request } from 'express';
+import jwt from 'jsonwebtoken';
+
 export type ServerError = Error & {
   errno: number;
   code: string;
   path: string;
   syscall: string;
   stack: string;
-}
+};
+
+export type Token = {
+  name: string;
+  email: string;
+  discord: string;
+  friend: string;
+  notification: string;
+  profile: string;
+  admin: boolean;
+};
+
+export type AuthRequest = Request & {
+  token: Token;
+};
+
+export type Sign = (
+  payload: string | object | Buffer,
+  secretOrPrivateKey: jwt.Secret,
+  options: jwt.SignOptions,
+) => string;
+
+export type Verify = (
+  token: string,
+  secretOrPublicKey: jwt.Secret,
+  options?: jwt.VerifyOptions,
+) => Token | undefined;
+
+export type Blacklist = (token: string) => void;
