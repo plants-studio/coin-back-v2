ecad000efddbde37dace16a5aa12c562989bf13c
al-co-js <shimjs8@naver.com>
2020-12-05 17:51:03 +0900

    feat: cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv-safe/config';
 
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
 import logger from 'morgan';
 import path from 'path';
@@ -9,10 +10,11 @@ import routes from './routes';
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', routes);
