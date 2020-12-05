469d0af0f131cd859b8468dcfc094fbc16054edd
al-co-js <shimjs8@naver.com>
2020-12-05 19:30:01 +0900

    fix: delete unnecessary syntax

--- src/types/node.d.ts
+++ src/types/node.d.ts
@@ -1,16 +1,14 @@
-declare global {
-  export namespace NodeJS {
-    export interface ProcessEnv {
-      NODE_ENV: 'development' | 'production';
-      ACCESS_SECRET: string;
-      REFRESH_SECRET: string;
-      PORT?: string;
-      DB_USER: string;
-      DB_PASSWORD: string;
-      DB_URI: string;
-      DISCORD_ID: string;
-      DISCORD_SECRET: string;
-      REDIRECT_URI: string;
-    }
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production';
+    ACCESS_SECRET: string;
+    REFRESH_SECRET: string;
+    PORT?: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB_URI: string;
+    DISCORD_ID: string;
+    DISCORD_SECRET: string;
+    REDIRECT_URI: string;
   }
 }
