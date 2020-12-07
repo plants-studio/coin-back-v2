65eec9fe9f66fb3cde46c3078c698e47f34accb9
al-co-js <shimjs8@naver.com>
2020-12-07 16:59:53 +0900

    refactor: may have undefined state

--- src/types/node.d.ts
+++ src/types/node.d.ts
@@ -1,14 +1,13 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NODE_ENV: 'development' | 'production';
-    ACCESS_SECRET: string;
-    REFRESH_SECRET: string;
+    ACCESS_SECRET?: string;
+    REFRESH_SECRET?: string;
     PORT?: string;
-    DB_USER: string;
-    DB_PASSWORD: string;
-    DB_URI: string;
-    DISCORD_ID: string;
-    DISCORD_SECRET: string;
-    REDIRECT_URI: string;
+    DB_USER?: string;
+    DB_PASSWORD?: string;
+    DB_URI?: string;
+    DISCORD_ID?: string;
+    DISCORD_SECRET?: string;
+    REDIRECT_URI?: string;
   }
 }
