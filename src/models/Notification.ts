1346e04ee9eccb68536c96197bc79643008c4963
al-co-js <shimjs8@naver.com>
2020-12-08 15:49:26 +0900

    feat: notification model

---
+++ src/models/Notification.ts
@@ -0,0 +1,17 @@
+import { createSchema, Type, typedModel } from 'ts-mongoose';
+
+const Notification = createSchema(
+  {
+    list: Type.array({ required: true }).of({
+      title: Type.string({ required: true }),
+      description: Type.string({ required: true }),
+      category: Type.string({ required: true }),
+    }),
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
+
+export default typedModel('Notification', Notification);
