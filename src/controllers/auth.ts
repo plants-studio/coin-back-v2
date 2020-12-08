e4640ac90f328bc7e492c4175a0637e8aec32a77
al-co-js <shimjs8@naver.com>
2020-12-08 15:48:11 +0900

    feat: refresh token api and revoke token api

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,15 +1,65 @@
 import { pbkdf2Sync, randomBytes } from 'crypto';
+import DiscordOAuth2 from 'discord-oauth2';
 import { Request, Response } from 'express';
 import { readFileSync } from 'fs';
 import path from 'path';
 import sharp from 'sharp';
 
 import hex from '../handlers/hex';
-import { sign } from '../handlers/jwt';
-import { Friend, Tag, User } from '../models';
+import { blacklist, sign, verify } from '../handlers/jwt';
+import {
+  Friend, Notification, Tag, User,
+} from '../models';
 import { SignInRequestBody, SignUpRequestBody } from '../types';
 
-export const signIn = async (req: Request, res: Response) => {
+const refresh = async (req: Request, res: Response) => {
+  if (req.headers.authorization?.startsWith('Bearer ')) {
+    const token = req.headers.authorization.substring(7);
+    if (token.split('.').length === 3) {
+      const verified = verify(token, process.env.REFRESH_SECRET!);
+      if (!verified) {
+        res.sendStatus(401);
+        return;
+      }
+      const accessToken = sign(verified, process.env.ACCESS_SECRET!, { expiresIn: '7h' });
+      const refreshToken = sign(verified, process.env.REFRESH_SECRET!, { expiresIn: '7d' });
+      res.status(200).send({ token: { accessToken, refreshToken } });
+    } else {
+      const oauth = new DiscordOAuth2();
+      const discord = await oauth.tokenRequest({
+        scope: 'identify',
+        grantType: 'refresh_token',
+        refreshToken: token,
+        clientId: process.env.DISCORD_ID,
+        clientSecret: process.env.DISCORD_SECRET,
+      });
+      res.status(200).send(discord);
+    }
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+const revoke = (req: Request, res: Response) => {
+  if (req.headers.authorization?.startsWith('Bearer ')) {
+    const token = req.headers.authorization.substring(7);
+    if (token.split('.').length === 3) {
+      blacklist(token);
+      res.sendStatus(200);
+    } else {
+      const oauth = new DiscordOAuth2();
+      oauth.revokeToken(
+        token,
+        Buffer.from(`${process.env.DISCORD_ID}:${process.env.DISCORD_SECRET}`).toString('base64'),
+      );
+      res.sendStatus(200);
+    }
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+const signIn = async (req: Request, res: Response) => {
   const { email, password }: SignInRequestBody = req.body;
 
   const user = await User.findOne({ email });
@@ -65,7 +115,7 @@ export const signIn = async (req: Request, res: Response) => {
   res.status(200).send({ token: { accessToken, refreshToken } });
 };
 
-export const signUp = async (req: Request, res: Response) => {
+const signUp = async (req: Request, res: Response) => {
   const {
     name, email, password, profile,
   }: SignUpRequestBody = req.body;
@@ -101,15 +151,22 @@ export const signUp = async (req: Request, res: Response) => {
 
   const newFriend = new Friend({ list: [] });
   await newFriend.save();
+  const newNotification = new Notification({ list: [] });
+  await newNotification.save();
 
   const newUser = new User({
     name: nameTag,
     email,
     password: `${encrypt}|${salt}`,
     friend: newFriend.id,
+    notification: newNotification.id,
     profile,
   });
 
   await newUser.save();
   res.status(200).send('계정을 성공적으로 생성했습니다');
+};
+
+export {
+  refresh, revoke, signIn, signUp,
 };
