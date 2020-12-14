3ad7bb97416408b5264d20eedd57e6f82282101c
al-co-js <shimjs8@naver.com>
2020-12-14 22:17:02 +0900

    feat: discord and edit api

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,7 @@
 import { pbkdf2Sync, randomBytes } from 'crypto';
 import DiscordOAuth2 from 'discord-oauth2';
 import { Request, Response } from 'express';
-import { readFileSync } from 'fs';
+import { readFileSync, unlinkSync } from 'fs';
 import path from 'path';
 import sharp from 'sharp';
 
@@ -10,7 +10,119 @@ import { blacklist, sign, verify } from '../handlers/jwt';
 import {
   Friend, Notification, Tag, User,
 } from '../models';
-import { SignInRequestBody, SignUpRequestBody } from '../types';
+import { AuthRequest, SignInRequestBody, SignUpRequestBody } from '../types';
+
+const discord = async (req: Request, res: Response) => {
+  if (req.headers.authorization?.startsWith('Bearer ')) {
+    const token = req.headers.authorization.substring(7);
+    const oauth = new DiscordOAuth2();
+    const user = await oauth.getUser(token);
+    if (!user.verified) {
+      res.sendStatus(401);
+      return;
+    }
+
+    if (!user.email) {
+      res.sendStatus(412);
+      return;
+    }
+
+    const emailCheck = await User.findOne({ email: user.email });
+    if (emailCheck) {
+      res.sendStatus(409);
+      return;
+    }
+
+    const userData = await User.findOne({ discord: user.id });
+    if (!userData) {
+      const nameTag = `${user.username}#${user.discriminator}`;
+      const newFriend = new Friend({ list: [] });
+      await newFriend.save();
+      const newNotification = new Notification({ list: [] });
+      await newNotification.save();
+      const newUser = new User({
+        email: user.email,
+        name: nameTag,
+        discord: user.id,
+        friend: newFriend.id,
+        notification: newNotification.id,
+        // TODO Profile with discord avatar
+      });
+      await newUser.save();
+      res.status(200).send({
+        email: user.email,
+        name: nameTag,
+        friend: newFriend.id,
+        notification: newNotification.id,
+        // TODO Profile with discord avatar
+      });
+      return;
+    }
+
+    res.status(200).send({
+      email: userData.email,
+      name: userData.name,
+      friend: userData.friend,
+      notification: userData.notification,
+      // TODO Profile with discord avatar
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+const edit = async (req: AuthRequest, res: Response) => {
+  const { token } = req;
+  const { name, profile } = req.body;
+
+  if (name.search('#') !== -1) {
+    res.status(412).send("닉네임에는 '#'이 포함될 수 없습니다");
+    return;
+  }
+
+  const user = await User.findById(token!.id);
+  if (!user) {
+    res.sendStatus(404);
+    return;
+  }
+
+  console.log(token);
+  if (name) {
+    let tag = await Tag.findOne({ name });
+    if (!tag) {
+      const newTag = new Tag({
+        name,
+      });
+      await newTag.save();
+      tag = newTag;
+    }
+    if (tag!.list.length === 0) {
+      res.status(409).send('닉네임이 너무 많이 사용되고 있어 더 이상 사용할 수 없습니다');
+      return;
+    }
+    const oldName = token!.name.split('#');
+    const oldTag = await Tag.findOne({ name: oldName[0] });
+    const nameTag = `${name}#${tag!.list[0]}`;
+    await oldTag!.updateOne({ $push: { list: Number.parseInt(oldName[1], 10) } });
+    await tag!.updateOne({ $pull: { list: tag!.list[0] } });
+    await user.updateOne({ name: nameTag });
+  }
+  if (profile) {
+    const id = Buffer.from(token!.id).toString('base64');
+    const dir = path.join(__dirname, '..', 'public', 'images', 'profiles');
+    const file = path.join(dir, `${id}.webp`);
+    try {
+      readFileSync(file);
+      unlinkSync(file);
+    } finally {
+      const buffer = profile.split(';base64,')[1];
+      await sharp(Buffer.from(buffer, 'base64')).resize(150, 150).webp().toFile(file);
+    }
+    await user.updateOne({ profile });
+  }
+
+  res.sendStatus(200);
+};
 
 const refresh = async (req: Request, res: Response) => {
   if (req.headers.authorization?.startsWith('Bearer ')) {
@@ -26,14 +138,14 @@ const refresh = async (req: Request, res: Response) => {
       res.status(200).send({ token: { accessToken, refreshToken } });
     } else {
       const oauth = new DiscordOAuth2();
-      const discord = await oauth.tokenRequest({
-        scope: 'identify',
+      const newToken = await oauth.tokenRequest({
+        scope: 'identify email',
         grantType: 'refresh_token',
         refreshToken: token,
         clientId: process.env.DISCORD_ID,
         clientSecret: process.env.DISCORD_SECRET,
       });
-      res.status(200).send(discord);
+      res.status(200).send(newToken);
     }
   } else {
     res.sendStatus(401);
@@ -68,7 +180,7 @@ const signIn = async (req: Request, res: Response) => {
     return;
   }
 
-  const temp = user.password.split('|');
+  const temp = user.password!.split('|');
   const encrypt = pbkdf2Sync(password, temp[1], 100000, 64, 'SHA512').toString('base64');
   if (temp[0] !== encrypt) {
     res.status(401).send('비밀번호가 옳지 않습니다');
@@ -86,12 +198,8 @@ const signIn = async (req: Request, res: Response) => {
       profile = `/images/profiles/${id}.webp`;
     } catch {
       const data = user.profile.split(';base64,')[1];
-      try {
-        await sharp(Buffer.from(data, 'base64')).resize(150, 150).webp().toFile(file);
-        profile = `/images/profiles/${id}.webp`;
-      } catch {
-        profile = `/images/profiles/default${number}.webp`;
-      }
+      await sharp(Buffer.from(data, 'base64')).resize(150, 150).webp().toFile(file);
+      profile = `/images/profiles/${id}.webp`;
     }
   } else {
     profile = `/images/profiles/default${number}.webp`;
@@ -139,12 +247,12 @@ const signUp = async (req: Request, res: Response) => {
     await newTag.save();
     tag = newTag;
   }
-  if (tag!.tags.length === 0) {
+  if (tag!.list.length === 0) {
     res.status(409).send('닉네임이 너무 많이 사용되고 있어 더 이상 사용할 수 없습니다');
     return;
   }
-  await tag!.updateOne({ $pull: { tags: tag!.tags[0] } });
-  const nameTag = `${name}#${tag!.tags[0]}`;
+  await tag!.updateOne({ $pull: { list: tag!.list[0] } });
+  const nameTag = `${name}#${tag!.list[0]}`;
 
   const salt = randomBytes(16).toString('base64');
   const encrypt = pbkdf2Sync(password, salt, 100000, 64, 'SHA512').toString('base64');
@@ -168,5 +276,5 @@ const signUp = async (req: Request, res: Response) => {
 };
 
 export {
-  refresh, revoke, signIn, signUp,
+  discord, edit, refresh, revoke, signIn, signUp,
 };
