ae9e0d27e9621d48625779b8c7589efac7356c9e
al-co-js <shimjs8@naver.com>
2020-12-15 07:56:07 +0900

    fix: promise

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -10,7 +10,7 @@ export default async (req: AuthRequest, res: Response, next: NextFunction) => {
     if (req.headers.authorization?.startsWith('Bearer ')) {
       const token = req.headers.authorization.substring(7);
       if (token.split('.').length === 3) {
-        const verified = verify(token, process.env.ACCESS_SECRET!);
+        const verified = await verify(token, process.env.ACCESS_SECRET!);
         if (!verified) {
           res.sendStatus(401);
           return;
