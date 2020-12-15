35066b2e16e192872378c192635cf59cde04003d
al-co-js <shimjs8@naver.com>
2020-12-15 22:41:25 +0900

    feat: api route

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
 
 import {
-  edit, refresh, revoke, signIn, signUp,
+  discord, edit, refresh, revoke, signIn, signUp,
 } from '../../controllers/auth';
 import auth from '../../middlewares/auth';
+import wrapAsync from '../../utils/wrapAsync';
 
 const router = Router();
 
-router.put('/', auth, edit);
-router.post('/refresh', refresh);
+router.put('/', wrapAsync(auth), edit);
+router.post('/discord', wrapAsync(discord));
+router.post('/refresh', wrapAsync(refresh));
 router.post('/revoke', revoke);
-router.post('/signin', signIn);
-router.post('/signup', signUp);
+router.post('/signin', wrapAsync(signIn));
+router.post('/signup', wrapAsync(signUp));
 
 export default router;
