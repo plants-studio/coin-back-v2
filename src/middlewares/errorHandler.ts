fe15462c41547d2bc26bf74978882ac4d39208f6
al-co-js <shimjs8@naver.com>
2020-12-16 23:05:06 +0900

    fix: error handler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,12 +1,14 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import { logger } from '../configs/winston';
 
-export default (err: any, req: Request, res: Response) => {
+export default (err: any, req: Request, res: Response, next: NextFunction) => {
   const errObj = {
     err,
     req,
   };
   logger.error(`Unknown Error : ${Date.now()}`, errObj);
+  console.log(`a${res}B`);
   res.status(500).send(err);
+  next(err);
 };
