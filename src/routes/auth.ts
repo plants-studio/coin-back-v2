a6def3a0cee3b9ab85ec55f67bc5beda968942f4
al-co-js <shimjs8@naver.com>
2020-12-17 20:09:53 +0900

    refactor: route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
 
 import {
-  discord, edit, refresh, revoke, signIn, signUp,
+  discord, refresh, revoke, signIn, signUp,
 } from '../controllers/auth';
-import auth from '../middlewares/auth';
 import wrapAsync from '../utils/wrapAsync';
 
 const router = Router();
 
-router.put('/', wrapAsync(auth), edit);
 router.post('/discord', wrapAsync(discord));
 router.post('/refresh', wrapAsync(refresh));
 router.post('/revoke', revoke);
