0a149c8ee688a2812718050131288c91cb35d05b
al-co-js <shimjs8@naver.com>
2020-12-17 11:18:31 +0900

    feat: default value

--- src/models/Comment.ts
+++ src/models/Comment.ts
@@ -2,7 +2,7 @@ import { createSchema, Type, typedModel } from 'ts-mongoose';
 
 const Comment = createSchema(
   {
-    list: Type.array({ required: true }).of({
+    list: Type.array({ required: true, default: [] }).of({
       writer: Type.string({ required: true }),
       content: Type.string({ required: true }),
     }),
